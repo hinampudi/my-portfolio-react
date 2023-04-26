f433d46ae01ae69bbff6fc55be4f995a91314836
Hrushidhar Inampudi <hinampudi2021@my.fit.edu>
2023-04-26 04:14:34 -0500

    Permalinks updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,30 +6,30 @@ import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">
+      <a href="/#" className="footer__logo">
         Hrushidhar
       </a>
       <ul className="permalinks">
         <li>
-          <a href="#">Home</a>
+          <a href="/#">Home</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="/#about">About</a>
         </li>
         <li>
-          <a href="#experience">Experience</a>
+          <a href="/#experience">Experience</a>
         </li>
         <li>
-          <a href="#services">Services</a>
+          <a href="/#services">Services</a>
         </li>
         <li>
-          <a href="#portfolio">Portfolio</a>
+          <a href="/#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="#testimonials">Testimonials</a>
+          <a href="/#testimonials">Testimonials</a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="/#contact">Contact</a>
         </li>
       </ul>
       <div className="footer__socials">
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,19 +11,19 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}>
+      <a href="/#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}>
         <AiOutlineHome />
       </a>
-      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}>
+      <a href="/#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}>
         <AiOutlineUser />
       </a>
-      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}>
+      <a href="/#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}>
         <AiOutlineBook />
       </a>
-      <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}>
+      <a href="/#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}>
         <MdOutlineMiscellaneousServices />
       </a>
-      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}>
+      <a href="/#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}>
         <AiOutlineMessage />
       </a>
     </nav>
